ad11a03430595c5b043c95aefae58d53c18f5c68
Stephen McCullough <me@swm.cc>
2026-02-27 05:17:44 +0000

    📦 - Add main entry point with global configuration

---
+++ lib/mailjunky.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative "mailjunky/version"
+require_relative "mailjunky/error"
+require_relative "mailjunky/configuration"
+require_relative "mailjunky/connection"
+require_relative "mailjunky/resources/base"
+require_relative "mailjunky/resources/emails"
+require_relative "mailjunky/client"
+
+module MailJunky
+  class << self
+    attr_writer :configuration
+
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def configure
+      yield(configuration)
+    end
+
+    def reset_configuration!
+      @configuration = Configuration.new
+    end
+  end
+end
